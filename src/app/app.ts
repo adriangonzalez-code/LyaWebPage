dccc6759f6b6b57cfc2aa8b56dd4c6415226cbac
Adrian <g.adrian.gonzalez.90@gmail.com>
2025-12-13 22:41:39 -0600

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../components/navbar.component';
+import { HeroComponent } from '../components/hero.component';
+import { ServicesComponent } from '../components/services.component';
+import { GalleryComponent } from '../components/gallery.component';
+import { ContactComponent } from '../components/contact.component';
+import { FooterComponent } from '../components/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    ServicesComponent,
+    GalleryComponent,
+    ContactComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('LyaStudio');
 }
