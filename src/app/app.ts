aa053b29473dd9d2bdb80d547d31e6d03c316ff4
Adrián González <g.adrian.gonzalez.90@gmail.com>
2025-12-15 22:47:32 -0600

    Add main layout components and update HTML structure for Lya Studio application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {Navbar} from './components/navbar/navbar';
+import {Hero} from './components/hero/hero';
+import {Services} from './components/services/services';
+import {About} from './components/about/about';
+import {Gallery} from './components/gallery/gallery';
+import {Testimonial} from './components/testimonial/testimonial';
+import {Contact} from './components/contact/contact';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    Navbar,
+    Hero,
+    Services,
+    About,
+    Gallery,
+    Testimonial,
+    Contact,
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
